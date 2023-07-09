a89956d04200544c72d28bf3d4b8ab395be8d23a
ToanDuong <duongthanhtoan980@gmail.com>
2023-07-09 23:13:07 +0700

    Update gRPC Basket API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +21,11 @@ builder.Services.AddStackExchangeRedisCache(option =>
 });
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (o => o.Address = new Uri(config.GetValue<string>("GrpcSettings:DiscountUrl")));
+
+builder.Services.AddScoped<DiscountGrpcService>();
 
 var app = builder.Build();
 
